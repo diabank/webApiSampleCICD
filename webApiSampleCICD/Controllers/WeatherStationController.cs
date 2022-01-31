87c1ec81a8c4b328627f6dc91d1c852536225902
diaban.kouame <diabankouame@gmail.com>
2022-01-30 20:27:06 -0600

    added new controller

---
+++ webApiSampleCICD/Controllers/WeatherStationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApiSampleCICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherStationController : ControllerBase
+    {
+        [HttpGet]
+        [Route("status")]
+        public IActionResult Get()
+        {
+            return Ok("we are here in WeatherStationController");
+        }
+    }
+}
